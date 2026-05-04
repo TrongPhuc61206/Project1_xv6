2d94fd6805a0122cd946e29adcf73e11af1f805c
VNThinh <vnthinh2418@clc.fitus.edu.vn>
2026-05-04 15:23:23 +0700

    Add usyscall allocation and pid initialization
    Allocate a page for usyscall and initialize pid.

--- kernel/proc.c
+++ kernel/proc.c
@@ -132,6 +132,16 @@ found:
     return 0;
   }
 
+  // Allocate a page for usyscall
+  if((p->usyscall = (struct usyscall *)kalloc()) == 0){
+    freeproc(p);
+    release(&p->lock);
+    return 0;
+  }
+
+  // Initialize pid value in this memory
+  p->usyscall->pid = p->pid;
+  
   // An empty user page table.
   p->pagetable = proc_pagetable(p);
   if(p->pagetable == 0){
